e5c425197c93fd7cb25cf9c1e19d103beb1fc1d9
Pulido27 <cpulido@ucol.mx>
2023-07-07 12:41:04 -0600

    arbol de busqueda con vectores

--- ArbolBusqueda/ArbolDeBusqueda.cpp
+++ ArbolBusqueda/ArbolDeBusqueda.cpp
@@ -1,47 +1,67 @@
 #include <iostream>
 
+using namespace std;
+
 // Definición de la clase Nodo 
 class Nodo {
 public:
   int valor;
-  Nodo* izquierdo;
-  Nodo* derecho;
+  Nodo* izquierdo = nullptr;
+  Nodo* derecho = nullptr;
 
   Nodo(int valor) {       // el valor pasado como parametro es diferente al valor anteriormente escrito
-    this->valor = valor;  // this es un puntero autoreferencial al mismo objeto que pertenece  
-    izquierdo = derecho = nullptr;
+    this->valor = valor;  // this es un puntero autoreferencial al mismo objeto que pertenece   
   }
 };
 
 // Definición de la clase ArbolBinarioBusqueda
 class ArbolBinarioBusqueda {
 private:
-  Nodo* raiz;
+  Nodo* raiz = nullptr;
 
 public:
   ArbolBinarioBusqueda() {
-    raiz = nullptr;
+    	
   }
 
-  // Función auxiliar para insertar un nodo en el árbol
-  Nodo* insertarNodo(Nodo* nodo, int valor) {
-    if (nodo == nullptr) {
-      return new Nodo(valor);
+void insertar(int valor) {
+  if (raiz == nullptr) {
+    raiz = new Nodo(valor);
+    return;
+  }
+
+  Nodo* nodoActual = raiz;
+  while (true) {
+    Nodo*& siguienteNodo = (valor < nodoActual->valor) ? nodoActual->izquierdo : nodoActual->derecho;
+    if (siguienteNodo == nullptr) {
+      siguienteNodo = new Nodo(valor);
+      return;
     }
+    nodoActual = siguienteNodo;
+  }
+}
 
-    if (valor < nodo->valor) {
-      nodo->izquierdo = insertarNodo(nodo->izquierdo, valor);
-    } else {
-      nodo->derecho = insertarNodo(nodo->derecho, valor);
+bool buscar(int valor){
+	if (raiz == nullptr) {
+    return false; 
+  }
+  
+  
+
+  Nodo* nodoActual = raiz;
+  while (true) {
+  	if(nodoActual->valor==valor){
+	  return true;  		
+	  }
+    Nodo*& siguienteNodo = (valor < nodoActual->valor) ? nodoActual->izquierdo : nodoActual->derecho;
+    if (siguienteNodo == nullptr) {
+      return false;
     }
+    nodoActual = siguienteNodo;
+  }	
+}
 
-    return nodo;
-  }
-
-  // Función para insertar un valor en el árbol
-  void insertar(int valor) {
-    raiz = insertarNodo(raiz, valor);
-  }
+  
 };
 
 // Ejemplo de uso
@@ -56,6 +76,18 @@ int main() {
   arbol.insertar(4);
   arbol.insertar(7);
   arbol.insertar(13);
+  
+  cout<< arbol.buscar(8);
+  cout<< arbol.buscar(3);
+  cout<< arbol.buscar(10);
+  cout<< arbol.buscar(1);
+  cout<< arbol.buscar(6);
+  cout<< arbol.buscar(14);
+  cout<< arbol.buscar(4);
+  cout<< arbol.buscar(7);
+  cout<< arbol.buscar(13);
+  cout<< arbol.buscar(25);	
+
 
   return 0;
 }
