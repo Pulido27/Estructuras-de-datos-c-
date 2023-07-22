f2647d3440b470914144843c3a78ebcd122b2a2a
Pulido27 <cpulido@ucol.mx>
2023-07-22 08:56:14 -0600

    final

---
+++ Invertir_cadena.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+//codigo apto solo cuando la palabra no termina en " "
+string invertir(string cadena) {
+	string palabra = "";
+	stack<string> miPila;
+	 
+	for (char c : cadena) {
+		if(c != ' '){
+			palabra += c;
+		}
+		else{
+			miPila.push(palabra);
+			palabra.clear();		
+		}
+	}
+	
+	
+	while (!miPila.empty()) {
+		palabra += " " + miPila.top();
+		miPila.pop();
+	}
+	
+	return palabra;
+}
+
+int main() {
+				
+	cout << invertir("asi habla yoda");
+	
+	return 0;
+}
